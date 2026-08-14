3a173a02965930fd604d11331f5192f6c40cd723
Fajar Nugraha <fajarnugraha9688@gmail.com>
2026-08-14 23:24:09 +0700

    Force HTTPS on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Setting;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         /*
         |--------------------------------------------------------------------------
         | SHARE DATA WEBSITE KE SEMUA BLADE
